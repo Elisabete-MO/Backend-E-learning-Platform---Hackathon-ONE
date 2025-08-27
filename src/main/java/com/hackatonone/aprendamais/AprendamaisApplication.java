f973e82a6723bd881c3ecca491a48cffecb4f292
Rangel <marcosvrangel@hotmail.com>
2025-08-27 17:30:15 -0300

    feat:(infra) configuração inicial do spring

---
+++ src/main/java/com/hackatonone/aprendamais/AprendamaisApplication.java
@@ -0,0 +1,13 @@
+package com.hackatonone.aprendamais;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AprendamaisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AprendamaisApplication.class, args);
+	}
+
+}
